7121e48fd6f3689231bf8bdb5e11872853e0a69d
oniseun <seunoni34@gmail.com>
2025-02-06 18:11:36 +0000

    feat: improve observation schema mapping

--- src/modules/observation/observation.schema.ts
+++ src/modules/observation/observation.schema.ts
@@ -1,22 +1,63 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-@Schema()
-export class Observation extends Document {
+class Coding {
   @Prop({ required: true })
-  id: string;
-
-  @Prop({ required: true })
-  status: string;
+  system: string;
 
   @Prop({ required: true })
   code: string;
 
+  @Prop()
+  display?: string;
+}
+
+class Code {
+  @Prop({ type: [Coding], required: true })
+  coding: Coding[];
+
+  @Prop()
+  text?: string;
+}
+
+class ValueQuantity {
   @Prop({ required: true })
-  issued: string;
+  value: number;
+
+  @Prop()
+  unit?: string;
+}
+
+class Subject {
+  @Prop({ required: true })
+  reference: string;
+}
+
+@Schema()
+export class Observation extends Document {
+  @Prop({ required: true })
+  resourceType: string;
+
+  @Prop({ required: true, unique: true, type: Number })
+  id: number;
 
   @Prop({ required: true })
-  patientId: string;
+  status: string;
+
+  @Prop({ type: Code, required: true })
+  code: Code;
+
+  @Prop({ type: Date, required: true })
+  issued: Date;
+
+  @Prop({ type: Subject, required: true })
+  subject: Subject;
+
+  @Prop()
+  comment?: string;
+
+  @Prop({ type: ValueQuantity })
+  valueQuantity?: ValueQuantity;
 }
 
 export const ObservationSchema = SchemaFactory.createForClass(Observation);
